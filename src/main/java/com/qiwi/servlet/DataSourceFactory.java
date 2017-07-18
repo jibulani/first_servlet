5a94401eb56c2114bc3c9e63b9eb9fae48e67978
Eugene <evgeni-trofimo@yandex.ru>
2017-07-18 11:51:02 +0300

    DataSourceFactory changed

--- src/main/java/com/qiwi/servlet/DataSourceFactory.java
+++ src/main/java/com/qiwi/servlet/DataSourceFactory.java
@@ -11,23 +11,18 @@ import org.apache.commons.pool.impl.GenericObjectPool;
 
 public class DataSourceFactory {
 
-    private static BasicDataSource dataSource;
 
     public static BasicDataSource getPostgreDataSource() {
 
-        if (dataSource == null) {
-            BasicDataSource ds = new BasicDataSource();
-            ds.setUrl("jdbc:postgresql://localhost:5432/etrofimov");
-            ds.setUsername("etrofimov");
-            ds.setPassword("1234");
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl("jdbc:postgresql://localhost:5432/etrofimov");
+        ds.setUsername("etrofimov");
+        ds.setPassword("1234");
 
-            ds.setMinIdle(5);
-            ds.setMaxIdle(10);
-            ds.setMaxOpenPreparedStatements(100);
+        ds.setMinIdle(5);
+        ds.setMaxIdle(10);
+        ds.setMaxOpenPreparedStatements(100);
 
-            dataSource = ds;
-        }
-
-        return dataSource;
+        return ds;
     }
 }
